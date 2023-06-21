f4be82c3b3bfd8effd343217294606258353379c
Josh <zylo.codes@gmail.com>
2023-06-20 21:02:13 -0500

    select DOM elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 // vars
 
 // select dom elements
-  // start button
-  // reset button
-  // count down timer
-  // word-blanks
-  // wins
-  // losses
+var startButton = document.querySelector('.start-button')
+var resetButton = document.querySelector('.reset-button')
+var timerEl = document.querySelector('.timer')
+var wordBlanksEl = document.querySelector('.word-blanks')
+var winsEl = document.querySelector('.win')
+var lossesEl = document.querySelector('.lose')
 
-  // list of words
+// list of words
   // userGuesses = [] list of letters guessed by user
   // answer = "function"
 
