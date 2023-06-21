11687d02187da2024540d4f91a2af2f9bf10ea8e
Josh <zylo.codes@gmail.com>
2023-06-20 21:12:09 -0500

    vars and start round listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,37 +9,40 @@ var winsEl = document.querySelector('.win')
 var lossesEl = document.querySelector('.lose')
 
 // list of words
-  // userGuesses = [] list of letters guessed by user
-  // answer = "function"
+var words = ["jellyfish", "pancakes", "callback", "bacon", "porcupine"]
+var userGuesses = [] // list of letters guessed by user
+var answer
 
-  function startRound() {
-    // start timer
-    // pick a random word
-    // set word-blanks to have _'s
-  }
+function startRound() {
+  console.log('Starting round...')
+  // start timer
+  // pick a random word
+  // set word-blanks to have _'s
+}
 
-  function onKeydown(event) {
-    // check if letter pressed is in the answer string
-      // if yes
-        // reveal letter in word-blanks
-  }
+function onKeydown(event) {
+  // check if letter pressed is in the answer string
+    // if yes
+      // reveal letter in word-blanks
+}
 
-  function tick() {
-    // check if userGuesses has all letters that are in answer string
-      // if has guess all letters
-        // end round
-        // increment wins
-        // display "you won!"
-      // else
-        // is our timer count at 0?
-          // if yes
-            // diplay "lost round"
-            // increment losses
-          // else
-            // decrement timer count
-  }
+function tick() {
+  // check if userGuesses has all letters that are in answer string
+    // if has guess all letters
+      // end round
+      // increment wins
+      // display "you won!"
+    // else
+      // is our timer count at 0?
+        // if yes
+          // diplay "lost round"
+          // increment losses
+        // else
+          // decrement timer count
+}
 
-  // Event Listeners
-    // start - startRound()
-    // reset
-    // keydown - onKeydown
+// Event Listeners
+// start - startRound()
+startButton.addEventListener('click', startRound)
+// reset
+// keydown - onKeydown
