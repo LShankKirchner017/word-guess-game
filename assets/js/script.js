7bb23438355d3093f63465ef597e0d7d24eaa6db
Lindsay <lshank017@gmail.com>
2023-07-05 10:54:26 -0500

    pseudo code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,45 @@
 // vars
 
 // select dom elements
-var startButton = document.querySelector('.start-button')
-var resetButton = document.querySelector('.reset-button')
-var timerEl = document.querySelector('.timer')
-var wordBlanksEl = document.querySelector('.word-blanks')
-var winsEl = document.querySelector('.win')
-var lossesEl = document.querySelector('.lose')
-console.log(words)
+  // start button 
+  // reset button
+  // count down timer
+  // word blanks
+  // wins
+  // losses
+
 // list of words
-var words = ["jellyfish", "pancakes", "callback", "bacon", "porcupine"]
-var userGuesses = [] // list of letters guessed by user
-var answer
+// userGuesses = [] list of letters guessed by user
+// answer = "function"
 
-function getRandomWord() {
-  var randomIdx = Math.floor( Math.random() * words.length )
-  answer = words[randomIdx]
-  console.log(answer)
+function startRound(){
+  // start timer
+  // pick a random word
+  // set word blanks to have underscores
 }
 
-function renderWord() {
-  var blanks = ""
-  for (var i = 0; i < answer.length; i++) {
-    var letter = answer[i]
-    // check if userGuesses contains current letter
-    if (userGuesses.includes(letter)) {
-      blanks += letter
-    } else {
-      blanks += "_"
-    }    
-  }
-  wordBlanksEl.innerText = blanks.split('').join(' ')
-}
-
-function startRound() {
-  // TODO: start timer
-  getRandomWord()
-  renderWord()
-}
-
-function onKeydown(event) {
+function onKeydown(event){
   // check if letter pressed is in the answer string
-  // and if not already guessed
-  if ( answer.includes(event.key) && !userGuesses.includes(event.key)  ) {
-    userGuesses.push(event.key)
-  }
-  renderWord()
+    // if yes
+      // reveal letter in word-blanks
 }
 
-function tick() {
-  // TODO:
-  // check if userGuesses has all letters that are in answer string
-    // if has guess all letters
+function tick(){
+  // check if userGuesses has all the letters that are in the answer string
+    // if has guessed all letters
       // end round
       // increment wins
-      // display "you won!"
+      // display "You won!"
     // else
-      // is our timer count at 0?
+      // is timer count at zero
         // if yes
-          // diplay "lost round"
+          // lost round
+          // display "You lost"
           // increment losses
-        // else
-          // decrement timer count
+      // else
+        // decrement timer count
 }
-
 // Event Listeners
-startButton.addEventListener('click', startRound)
-
-document.body.addEventListener('keypress', onKeydown)
-
-// TODO: reset
+  // start
+  // reset
+  // keydown - onKeydown
