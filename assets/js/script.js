19eeef78bdc499a192f9891408a2d0ac17076900
Lindsay <lshank017@gmail.com>
2023-07-05 13:39:24 -0500

    timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,19 @@
 // select dom elements
   var startButton = document.querySelector(".start-button")
   var resetButton = document.querySelector(".reset-button");
-  var timerEl = document.querySelector(".timer");
+  var timerEl = document.querySelector(".timer-count");
   var wordBlanksEl = document.querySelector(".word-blanks");
   var winsEl = document.querySelector(".win");
   var lossesEl = document.querySelector(".lose");
+  var wins = 0
+  var losses = 0
+  var time = 10
 
 // list of words
 var words = ["jellyfish", "pancakes", "callback", "bacon", "porcupine"]
 var userGuesses = [] // list of letters guessed by user
 var answer // undefined until start button is hit
+var intervalId
 
 function getRandomWord(){
  var randomIdx = Math.floor(Math.random() * words.length);
@@ -35,12 +39,35 @@ function renderWord(){
   wordBlanksEl.innerText = blanks.split("").join(" ");
 }
 
+function tick() {
+  console.log("tick...")
+  // TODO:
+  // check if userGuesses has all the letters that are in the answer string
+  // if has guessed all letters
+  if(!wordBlanksEl.innerText.includes("_")){
+    // end round
+    wins++
+    wordBlanksEl.innerText = "You won! 🥳"
+  } else {
+    // are we out of time?
+    if (time <= 0){
+      clearInterval(intervalId)
+      timerEl.innerText = "Out of time!"
+      // display "You lost"
+      // increment losses
+    } else {
+      // decrement timer count
+      time--
+      timerEl.innerText = time
+    }
+    // display count in page
+  }
+}
 
 function startRound(){
-  console.log("Starting round...")
-  // TODO: start timer 
   getRandomWord()
   renderWord()
+  intervalId = setInterval(tick, 1000)
 }
 
 function onKeydown(event){ 
@@ -55,21 +82,7 @@ function onKeydown(event){
     renderWord()
 }
 
-function tick(){
-  // check if userGuesses has all the letters that are in the answer string
-    // if has guessed all letters
-      // end round
-      // increment wins
-      // display "You won!"
-    // else
-      // is timer count at zero
-        // if yes
-          // lost round
-          // display "You lost"
-          // increment losses
-      // else
-        // decrement timer count
-}
+
 // Event Listeners
 startButton.addEventListener('click', startRound)
 
